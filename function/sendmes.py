0e6554a5c504a2dbaf0a8c7b4c62648f2ceaf511
VladislavBakshanskij <vlad.bakshanskij@gmail.com>
2023-05-16 11:31:04 +0300

    add user-agent headers

--- function/sendmes.py
+++ function/sendmes.py
@@ -8,10 +8,16 @@ def sendmes_telegram(text: str, chat_id):
 
     method = URL + os.getenv('TOKEN') + "/sendMessage"
 
-    r = requests.post(method, data={
+    body = {
         "chat_id": chat_id,
         "text": text,
         "parse_mode": "markdown",
         "disable_web_page_preview": "true"
-        })
-    validate(r)
+    }
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0"
+    }
+
+    response = requests.post(method, data=body, headers=headers)
+    validate(response)
